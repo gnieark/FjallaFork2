5255480a2fee27bf4e91b2e55f6c44799e4d5e84
gnieark <gnieark@free.fr>
2017-03-29 22:49:38 +0200

    Initial Fork

---
+++ _define.php
@@ -0,0 +1,16 @@
+<?php
+/*  -- BEGIN LICENSE BLOCK ---------------------------------------
+This is part of theme Fialla for Dotclear 2 blog
+# Copyright (c) 2014
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------*/
+
+$this->registerModule(
+	/* Name */			"Fjalla",
+	/* Description*/		"A responsive Dotclear theme",
+	/* Author */			"ben_griffith",
+	/* Version */			'2.0.1'
+);
